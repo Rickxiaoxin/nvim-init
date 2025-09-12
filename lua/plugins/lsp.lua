a4fbb62627ac8f0d8a07e8e6cb9d89d470644d1c
WeiShx <2642138150@qq.com>
2025-09-12 22:39:10 +0800

    增加.gitignore、theme配置、vimtex插件，修改lsp配置

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,2 +1,26 @@
+local servers = { "lua_ls", "texlab", "basedpyright", "clangd" }
+
 return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    config = function()
+      vim.lsp.enable(servers)
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    build = ":MasonUpdate",
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = servers,
+    },
+  },
 }
+
