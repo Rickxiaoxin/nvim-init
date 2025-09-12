fc2439706e891e6dcd800f29b3a984a820053ee2
WeiShx <2642138150@qq.com>
2025-09-13 02:40:05 +0800

    insert不改变窗口尺寸,增加treesitter和diagnostic配置

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'powershell', 'python', 'query', 'vim', 'vimdoc' },
+    auto_install = false,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      -- additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = {
+      enable = true,
+      -- disable = {},
+    },
+  },
+}
+
